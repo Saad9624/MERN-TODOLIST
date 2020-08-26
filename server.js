a88ec819cf99495bef17a2a2ef0fb4e8a19f42bb
Saad\saadsheikh <saadsheikh1996@yahoo.com>
2020-08-26 10:44:18 -0700

    deployed on heroku

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require();
 
 const items = require('./routes/api/items');
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*' , (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client' ,'build', 'index.html'));        
+    } );
+}
+
 const app = express() ;
 
 app.use(bodyParser.json()) ;
